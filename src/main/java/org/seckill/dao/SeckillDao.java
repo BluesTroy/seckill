468a31cb29a95a3d38e33f464d2e99ac678cacf8
Troy <zhuyongzhen1219@qq.com>
2016-10-01 23:07:56 +0800

    添加测试代码

--- src/main/java/org/seckill/dao/SeckillDao.java
+++ src/main/java/org/seckill/dao/SeckillDao.java
@@ -1,5 +1,6 @@
 package org.seckill.dao;
 
+import org.apache.ibatis.annotations.Param;
 import org.seckill.entity.Seckill;
 
 import java.util.Date;
@@ -8,6 +9,7 @@ import java.util.List;
 /**
  * Created by Troy on 2016/10/1.
  */
+//ctrl+shift+T 创建单元测试快捷键。要先选中接口名
 public interface SeckillDao {
 
     /**
@@ -16,7 +18,7 @@ public interface SeckillDao {
      * @param killTime
      * @return 如果影响行数>1，表示更新的记录行数
      */
-    int reduceNumber(long seckillId, Date killTime);
+    int reduceNumber(@Param("seckillId") long seckillId, @Param("killTime") Date killTime);
 
     /**
      * 根据id查询秒杀对象
@@ -31,6 +33,6 @@ public interface SeckillDao {
      * @param limit
      * @return
      */
-    List<Seckill> queryAll(int offset, int limit);
-
+    List<Seckill> queryAll(@Param("offset") int offset, @Param("limit") int limit);
+    //java没有保存形参的记录：queryAll(int offset,int limit)->queryAll(arg0,arg1)
 }
---
+++ src/main/test/java/org/seckill/dao/SeckillDaoTest.java
@@ -0,0 +1,56 @@
+package org.seckill.dao;
+
+import org.junit.runner.RunWith;
+import org.seckill.entity.Seckill;
+import org.springframework.context.Lifecycle;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Troy on 2016/10/1.
+ */
+
+/**
+ * 配置spring和junit整合，junit启动时加载springIOC容器
+ * spring-test，junit
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+//告诉junit spring配置文件
+@ContextConfiguration({"classpath:spring/spring-dao.xml"})
+public class SeckillDaoTest {
+    //注入Dao实现类依赖
+    @Resource
+    private SeckillDao seckillDao;
+
+    @org.junit.Test
+    public void testReduceNumber() throws Exception {
+        long id = 1000;
+        Seckill seckill = seckillDao.queryById(id);
+        System.out.println(seckill.getName());
+        System.out.println(seckill);
+
+    }
+
+    @org.junit.Test
+    public void testQueryById() throws Exception {
+        //java没有保存形参的记录：queryAll(int offset,int limit)->queryAll(arg0,arg1)
+        List<Seckill> seckills = seckillDao.queryAll(0, 100);
+        for (Seckill seckill : seckills) {
+            System.out.println(seckill);
+        }
+    }
+
+    @org.junit.Test
+    public void testQueryAll() throws Exception {
+        Date killTime = new Date();
+        int updateCount=seckillDao.reduceNumber(1000L,killTime);
+        System.out.println("updateCount="+updateCount);
+    }
+}
