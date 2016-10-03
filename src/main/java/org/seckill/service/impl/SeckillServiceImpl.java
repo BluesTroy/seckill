bdfa0e56ea6b7addf2dffc3923fb954ad1c1c52f
Troy <zhuyongzhen1219@qq.com>
2016-10-03 13:34:25 +0800

    配置spring事务管理器

--- src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
+++ src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
@@ -15,6 +15,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.DigestUtils;
 
 import java.util.Date;
@@ -69,6 +70,13 @@ public class SeckillServiceImpl implements SeckillService {
         return md5;
     }
 
+    @Transactional
+    /**
+     * 使用注解控制事务方法的有点：
+     * 1、开发团队达成一致约定，明确标注事务方法的编程风格
+     * 2、保证事务方法的执行时间尽可能短，不要穿插其他网络 操作 RPC/HTTP请求或者剥离到事务方法外部.
+     * 3、不是所有的方法都需要事务。如只有一条修改操作，只读操作不需要事务控制
+     */
     public SeckillExcution executeSeckill(long seckillId, long userPhone, String md5)
             throws SeckillException, RepeatKillException, SeckillCloseException {
         if (md5 == null || !md5.equals(getMD5(seckillId))){
